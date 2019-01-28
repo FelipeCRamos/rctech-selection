e7340cc43325b024f21fc53ed56ae6854ec0dcd5
Felipe Ramos <felipecramos00@gmail.com>
2019-01-28 00:02:58 -0300

    Removed unnecessary classes

--- website/home/management/commands/capturar_noticias.py
+++ website/home/management/commands/capturar_noticias.py
@@ -42,39 +42,6 @@ class Tecmundo:
         else:
             return news_obj
 
-class TecmundoNews:
-    '''
-    Different API to fetch from news page on tecmundo
-    '''
-    def __init__(self):
-        self.link = 'https://www.tecmundo.com.br/novidades?page='
-        self.page = 1
-        self.info = [Post(item[1], item[0]) for item in self.fetch()]
-
-    def fetch(self):
-        pattern = re.compile(r'''<h3\sclass="nzn-news-title\suk-margin-remove"[^>]+><a data-bind="[^"]+"\shref="([^"]*)">([^<]+)''')
-
-        news_obj = []
-
-        for i in range(1,6):
-            print("Fetching page {}...".format(i))
-            try:
-                c_page = urlopen(Request(self.link+str(i))).read().decode('UTF-8')
-                print("\tPAGE: {}".format(self.link+str(i)))
-                log_f = open('log.html', 'w')
-                log_f.write(c_page)
-            except Exception as e:
-                print(e)
-                exit()
-
-            found = re.findall(pattern, c_page)
-            print(found)
-            for new in found:
-                print("Appending: {}".new[1])
-                #  news_obj.append(new)
-
-        return news_obj
-
 class Command(BaseCommand):
     help = 'Irá alimentar o banco de dados com as noticias atuais'
 
@@ -91,7 +58,6 @@ class Command(BaseCommand):
 
         print("Starting to fetch from Tecmundo")
         news_titles = Tecmundo().info
-        #  news_titles = TecmundoNews().info
 
         print("Fetched successfully, now inserting into the database")
         print("-"*50)
